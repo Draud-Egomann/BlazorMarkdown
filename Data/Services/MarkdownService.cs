1fa1e544eddac6cf2aa6c76aa49f37558be52c36
Justin <justin-urbanek@bluewin.ch>
2024-06-10 16:14:50 +0200

    feat: Markdown Service

---
+++ Data/Services/MarkdownService.cs
@@ -0,0 +1,27 @@
+﻿using Markdig;
+
+namespace Data.Services;
+
+public class MarkdownService {
+  public string RenderMarkdown(string markdown) {
+    if (string.IsNullOrWhiteSpace(markdown))
+      return string.Empty;
+
+    var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+    return Markdown.ToHtml(markdown, pipeline);
+  }
+
+  public string GetMarkdownContent(string path) {
+    if (string.IsNullOrWhiteSpace(path))
+      return string.Empty;
+
+    try {
+      path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", path);
+
+      var content = File.ReadAllText(path);
+      return RenderMarkdown(content);
+    } catch (Exception ex) {
+      return $"<p class=\"text-danger\">Error loading markdown file: {ex.Message}</p>";
+    }
+  }
+}
